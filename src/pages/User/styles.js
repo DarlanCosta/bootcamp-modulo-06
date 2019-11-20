c91b49dfc96a219915f19608535d0c16765c9211
DarlanCosta <darlandsv@gmail.com>
2019-11-20 16:14:34 -0300

    first commit

---
+++ src/pages/User/styles.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View``;
+
+export const Header = styled.View`
+  align-items: center;
+  padding-bottom: 20px;
+  border-bottom-width: 1px;
+  border-color: #eee;
+`;
+
+export const Avatar = styled.Image`
+  width: 100px;
+  height: 100px;
+  border-radius: 50px;
+  background: #eee;
+`;
+
+export const Name = styled.Text`
+  font-size: 20px;
+  color: #333;
+  font-weight: bold;
+  margin-top: 10px;
+  text-align: center;
+`;
+
+export const Bio = styled.Text`
+  font-size: 14px;
+  line-height: 18px;
+  color: #999;
+  margin-top: 5px;
+  text-align: center;
+`;
